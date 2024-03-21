3474b1a28db33949f2bc9d90cd0af6a8281c4ee7
Carl GENNETAIS <carl.gennetais@pm.me>
2024-03-21 19:00:26 +0100

    create project strucutre with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='OpenClassrooms Projet 8 : Déployer un modèle dans le cloud',
+    author='carl',
+    license='',
+)
